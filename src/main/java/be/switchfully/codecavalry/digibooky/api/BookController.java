0166f8164847d40e11736febf6d6dfc8f1b5bc0c
Pascal <pascal.baelen@live.com>
2020-10-21 15:49:35 +0200

    GetBookController

--- src/main/java/be/switchfully/codecavalry/digibooky/api/BookController.java
+++ src/main/java/be/switchfully/codecavalry/digibooky/api/BookController.java
@@ -1,9 +1,30 @@
 package be.switchfully.codecavalry.digibooky.api;
 
+import be.switchfully.codecavalry.digibooky.service.BookService;
+import be.switchfully.codecavalry.digibooky.service.dto.BookDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
+@RequestMapping(path = "/books")
 public class BookController {
+    private BookService bookService;
 
+    @Autowired
+    public BookController(BookService bookService) {
+        this.bookService = bookService;
+    }
 
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseStatus(HttpStatus.OK)
+    public List<BookDTO> getAllBooks() {
+        return bookService.getAllBookDTOs();
+    }
 }
