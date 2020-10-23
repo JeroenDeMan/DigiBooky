adbfd18873dffe2e0381ae4ee6e53bdeb383b59b
Alex-Schouten <alexandraschouten@hotmail.com>
2020-10-23 14:53:37 +0200

    testing librarian and admin lists

---
+++ src/test/java/be/switchfully/codecavalry/digibooky/business/repository/AdminRepositoryTest.java
@@ -0,0 +1,59 @@
+package be.switchfully.codecavalry.digibooky.business.repository;
+
+import be.switchfully.codecavalry.digibooky.business.entity.users.Admin;
+import be.switchfully.codecavalry.digibooky.exceptions.users.AdminAlreadyExistException;
+import be.switchfully.codecavalry.digibooky.util.MailAddress;
+import be.switchfully.codecavalry.digibooky.util.SocialSecurityNumber;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class AdminRepositoryTest {
+    private Admin admin;
+    private Admin admin2;
+    private AdminRepository repository;
+
+    @BeforeEach
+    public void setUp(){
+        SocialSecurityNumber socialSecurityNumber = new SocialSecurityNumber("121212-123-12");
+        MailAddress mailAddress = new MailAddress("MyEmail@gmail.com");
+        SocialSecurityNumber socialSecurityNumber2 = new SocialSecurityNumber("121212-123-15");
+        MailAddress mailAddress2 = new MailAddress("MyEmails@gmail.com");
+
+        admin = new Admin(socialSecurityNumber, "Jeroen", "De Man", mailAddress);
+        admin2 = new Admin(socialSecurityNumber2, "Alex", "Schouten", mailAddress2);
+
+        repository = new AdminRepository();
+
+    }
+
+    @Test
+    public void adminAlreadyExist_ThrowsAdminAlreadyExistException(){
+        repository.save(admin);
+        Assertions.assertThrows(AdminAlreadyExistException.class, ()-> repository.save(admin));
+
+    }
+
+    @Test
+    public void saveAdmin_ReturnsSameAdmin(){
+        Assertions.assertEquals(admin, repository.save(admin));
+    }
+
+    @Test
+    public void whenOfferingTwoAdmins_ReturnListOfTwoAdmins(){
+        List<Admin> listOfAdmins = new ArrayList<>();
+
+        listOfAdmins.add(new Admin(new SocialSecurityNumber("1342549-325-15"), "Adrie", "Admin", new MailAddress("adrie@admin.be")));
+        listOfAdmins.add(admin);
+        listOfAdmins.add(admin2);
+
+        repository.save(admin);
+        repository.save(admin2);
+
+        Assertions.assertEquals(listOfAdmins.size(), repository.getAdmins().size());
+    }
+
+}
---
+++ src/test/java/be/switchfully/codecavalry/digibooky/business/repository/LibrarianRepositoryTest.java
@@ -0,0 +1,59 @@
+package be.switchfully.codecavalry.digibooky.business.repository;
+
+import be.switchfully.codecavalry.digibooky.business.entity.users.Librarian;
+import be.switchfully.codecavalry.digibooky.exceptions.users.LibrarianAlreadyExistException;
+import be.switchfully.codecavalry.digibooky.util.MailAddress;
+import be.switchfully.codecavalry.digibooky.util.SocialSecurityNumber;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class LibrarianRepositoryTest {
+    private Librarian librarian;
+    private Librarian librarian2;
+    private LibrarianRepository repository;
+
+    @BeforeEach
+    public void setUp(){
+        SocialSecurityNumber socialSecurityNumber = new SocialSecurityNumber("121212-123-12");
+        MailAddress mailAddress = new MailAddress("MyEmail@gmail.com");
+        SocialSecurityNumber socialSecurityNumber2 = new SocialSecurityNumber("121212-123-22");
+        MailAddress mailAddress2 = new MailAddress("MyEmailtje@gmail.com");
+
+        librarian = new Librarian(socialSecurityNumber, "Jeroen", "De Man", mailAddress);
+
+        librarian2 = new Librarian(socialSecurityNumber2, "Lisa", "Hammouri", mailAddress2);
+
+        repository = new LibrarianRepository();
+
+    }
+
+    @Test
+    public void librarianAlreadyExist_ThrowsLibrarianAlreadyExistException(){
+        repository.save(librarian);
+        Assertions.assertThrows(LibrarianAlreadyExistException.class, ()-> repository.save(librarian));
+
+    }
+
+    @Test
+    public void saveLibrarian_ReturnsSameLibrarian(){
+        Assertions.assertEquals(librarian, repository.save(librarian));
+    }
+
+    @Test
+    public void whenOfferingTwoLibrarians_ReturnListOfTwoLibrarians(){
+        List<Librarian> listOfLibrarians = new ArrayList<>();
+
+        listOfLibrarians.add(librarian);
+        listOfLibrarians.add(librarian2);
+
+        repository.save(librarian);
+        repository.save(librarian2);
+
+        Assertions.assertEquals(listOfLibrarians.size(), repository.getLibrarians().size());
+    }
+
+}
